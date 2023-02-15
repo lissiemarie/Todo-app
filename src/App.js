2f3662ccbd1a001e1c0aabb46567079c6358808c
lissiemarie <elyssamunden@gmail.com>
2023-02-15 12:54:21 -0700

    Init push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>To-do List</h1>
+        <div className='todo-list'>
+          
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
